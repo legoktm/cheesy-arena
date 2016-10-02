dc3186e848585d9032f172dc8577ffa952c90e67
Kunal Mehta <legoktm@member.fsf.org>
2016-10-01 20:00:04 -0700

    Sponsor slides, get rid of subtitle from CSS
    $ sqlite3 event.db < import_sponsor_slides.sql

---
+++ import_sponsor_slides.sql
@@ -0,0 +1,11 @@
+DELETE FROM "sponsor_slides";
+INSERT INTO "sponsor_slides" VALUES(1,'','','','wrrf.png',5);
+INSERT INTO "sponsor_slides" VALUES(2,'','','','ty_evhs.png',5);
+INSERT INTO "sponsor_slides" VALUES(3,'','','','ty_volunteers17.png',5);
+INSERT INTO "sponsor_slides" VALUES(4,'','','','ty_sponsors17.png',5);
+INSERT INTO "sponsor_slides" VALUES(5,'','','','firstca.png',8);
+INSERT INTO "sponsor_slides" VALUES(6,'','','','google.png',8);
+INSERT INTO "sponsor_slides" VALUES(7,'','','','lockheed.png',8);
+INSERT INTO "sponsor_slides" VALUES(8,'','','','neato.png',8);
+INSERT INTO "sponsor_slides" VALUES(9,'','','','pods.png',6);
+
