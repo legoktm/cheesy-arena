73033aeff862a3b83973d28ea9de8d3e3574779a
Kunal Mehta <legoktm@member.fsf.org>
2016-10-07 01:52:34 -0700

    Super hacky audience selection of defenses in playoffs
    It'll work.

--- setup_defense_selection.go
+++ setup_defense_selection.go
@@ -7,7 +7,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"math/rand"
 	"net/http"
+	"os"
 	"strconv"
 	"text/template"
 )
@@ -34,8 +37,16 @@ func DefenseSelectionPostHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	redErr := validateDefenseSelection([]string{r.PostFormValue("redDefense2"),
-		r.PostFormValue("redDefense3"), r.PostFormValue("redDefense4"), r.PostFormValue("redDefense5")})
+	// Make sure audience selected defense is the same for both.
+	if r.PostFormValue("redDefense3") != r.PostFormValue("blueDefense3") &&
+		r.PostFormValue("redDefense3") != "" &&
+		r.PostFormValue("blueDefense3") != "" {
+		renderDefenseSelection(w, r, "Audience-selected defenses are not the same!")
+		return
+	}
+
+	redErr := validateDefenseSelection([]string{r.PostFormValue("redDefense3"),
+		r.PostFormValue("redDefense4"), r.PostFormValue("redDefense5")})
 	if redErr == nil {
 		match.RedDefense1 = "LB"
 		match.RedDefense2 = "CDF"
@@ -43,8 +54,9 @@ func DefenseSelectionPostHandler(w http.ResponseWriter, r *http.Request) {
 		match.RedDefense4 = r.PostFormValue("redDefense4")
 		match.RedDefense5 = r.PostFormValue("redDefense5")
 	}
-	blueErr := validateDefenseSelection([]string{r.PostFormValue("blueDefense2"),
-		r.PostFormValue("blueDefense3"), r.PostFormValue("blueDefense4"), r.PostFormValue("blueDefense5")})
+	blueErr := validateDefenseSelection([]string{r.PostFormValue("blueDefense3"),
+		r.PostFormValue("blueDefense4"), r.PostFormValue("blueDefense5")})
+
 	if blueErr == nil {
 		match.BlueDefense1 = "LB"
 		match.BlueDefense2 = "CDF"
@@ -72,6 +84,15 @@ func DefenseSelectionPostHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/setup/defense_selection", 302)
 }
 
+// c = 3, d = 4, e = 5
+func incrDefenseGroup(group int) int {
+	group++
+	if group > 5 {
+		group = 3
+	}
+	return group
+}
+
 func renderDefenseSelection(w http.ResponseWriter, r *http.Request, errorMessage string) {
 	template := template.New("").Funcs(templateHelpers)
 	_, err := template.ParseFiles("templates/setup_defense_selection.html")
@@ -79,6 +100,30 @@ func renderDefenseSelection(w http.ResponseWriter, r *http.Request, errorMessage
 		handleWebErr(w, err)
 		return
 	}
+
+	var start int
+
+	// Lazy, don't create a whole db structure for this...
+	fname := "defense.txt"
+	if _, err := os.Stat(fname); err == nil {
+		b, err := ioutil.ReadFile(fname)
+		if err != nil {
+			panic(err)
+		}
+		start, _ = strconv.Atoi(string(b))
+	} else {
+		start = rand.Intn(3) + 3
+		err = ioutil.WriteFile(fname, []byte(strconv.Itoa(start)), 0644)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// QF = 4, SF = 2, F = 1, reset every eliminstance
+	humanName := map[int]string{4: "QF", 2: "SF", 1: "F"}
+	audDefPick := make(map[string]string)
+	defGroup := map[int]string{3: "C (M, R)", 4: "D (SP, DB)", 5: "E (RT, RW)"}
+	audDefPick["QF round 1"] = defGroup[start]
 
 	matches, err := db.GetMatchesByType("elimination")
 	if err != nil {
@@ -90,14 +135,23 @@ func renderDefenseSelection(w http.ResponseWriter, r *http.Request, errorMessage
 		if match.Status != "complete" {
 			unplayedMatches = append(unplayedMatches, match)
 		}
+		groupStr := fmt.Sprintf("%s round %d", humanName[match.ElimRound], match.ElimInstance)
+		fmt.Println(groupStr)
+		_, ok := audDefPick[groupStr]
+		if !ok {
+			// Not set
+			start = incrDefenseGroup(start)
+			audDefPick[groupStr] = defGroup[start]
+		}
 	}
 
 	data := struct {
 		*EventSettings
-		Matches      []Match
-		DefenseNames map[string]string
-		ErrorMessage string
-	}{eventSettings, unplayedMatches, defenseNames, errorMessage}
+		Matches        []Match
+		DefenseNames   map[string]string
+		ErrorMessage   string
+		RandomDefenses map[string]string
+	}{eventSettings, unplayedMatches, defenseNames, errorMessage, audDefPick}
 	err = template.ExecuteTemplate(w, "setup_defense_selection.html", data)
 	if err != nil {
 		handleWebErr(w, err)
@@ -105,11 +159,24 @@ func renderDefenseSelection(w http.ResponseWriter, r *http.Request, errorMessage
 	}
 }
 
-// Takes a slice of the defenses in positions 2-5 and returns an error if they are not valid.
+func inSet(defense string, defenses []string) bool {
+	for _, name := range defenses {
+		if name == defense {
+			return true
+		}
+	}
+
+	return false
+}
+
+// Takes a slice of the defenses in positions 3-5 and returns an error if they are not valid.
 func validateDefenseSelection(defenses []string) error {
 	// Build map to track which defenses have been used.
 	// FIXME UPDATE THIS
 	defenseCounts := make(map[string]int)
+	cCounts := 0
+	dCounts := 0
+	eCounts := 0
 	for _, defense := range placeableDefenses {
 		defenseCounts[defense] = 0
 	}
@@ -126,12 +193,30 @@ func validateDefenseSelection(defenses []string) error {
 			return fmt.Errorf("Invalid defense type: %s", defense)
 		}
 		if defenseCount != 0 {
-			return fmt.Errorf("Defense used more than once: %s", defense)
+			return fmt.Errorf("Defense used more than once: %s", defenseNames[defense])
+		}
+		if inSet(defense, cDefenses) {
+			if cCounts != 0 {
+				return fmt.Errorf("Can only use one defense from group C (M, R)")
+			}
+			cCounts++
+		}
+		if inSet(defense, dDefenses) {
+			if dCounts != 0 {
+				return fmt.Errorf("Can only use one defense from group D (SP, DB)")
+			}
+			dCounts++
+		}
+		if inSet(defense, eDefenses) {
+			if eCounts != 0 {
+				return fmt.Errorf("Can only use one defense from group E (RT, RW)")
+			}
+			eCounts++
 		}
 		defenseCounts[defense]++
 	}
 
-	if numBlankDefenses > 0 && numBlankDefenses < 4 {
+	if numBlankDefenses > 0 && numBlankDefenses < 3 {
 		return fmt.Errorf("Cannot leave defenses blank.")
 	}
 
